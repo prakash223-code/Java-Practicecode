97a09d457512c60327f1d9c2ae6230d018e88aa3
sms-enterprises <sms-enterprises@sms-enterprises-H610M-H-V2-DDR4>
2025-12-29 17:13:26 +0530

    Try Catch block Created

---
+++ src/ExceptionHandling/TryCatch.java
@@ -0,0 +1,15 @@
+package ExceptionHandling;
+
+public class TryCatch {
+	public static void main(String[] args) {
+		try {
+			int a = 10;
+			int b = 2;
+
+			System.out.println(a / b);
+		} catch (Exception e) {
+			System.out.println("Divided by zero is undefined");
+		}
+		System.out.println("<--Program Ends-->");
+	}
+}
