550a3a4e628f36803f230d60514746507c8751cc
Prakash-dev <prakashdesikan50@gmail.com>
2025-12-31 15:30:31 +0530

    For Each Loop Created

---
+++ src/Loops/ForEach.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class ForEach {
+	public static void main(String[] args) {
+		int numbers[]= {12,34,56,78,90};
+		
+		for(int num:numbers) {
+			System.out.println(num);
+		}
+	}
+}
