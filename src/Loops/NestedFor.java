d755c5ac52acb0526a96d52d9931f96c7b479846
Prakash-dev <prakashdesikan50@gmail.com>
2025-12-31 14:34:20 +0530

    Nested For loop Created

---
+++ src/Loops/NestedFor.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class NestedFor {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int count=1;count<=2;count++) {
+				System.out.println("java");
+			}
+		}
+	}
+}
